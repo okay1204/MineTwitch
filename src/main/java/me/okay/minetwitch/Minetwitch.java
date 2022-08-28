ab84f4951d9794435ccb6a010902b6db13e9b29e
okayyyy <zghan2005@gmail.com>
2022-08-27 21:48:51 -0700

    Add database and potential fix to random twitch4j errors

--- src/main/java/me/okay/minetwitch/Minetwitch.java
+++ src/main/java/me/okay/minetwitch/Minetwitch.java
@@ -2,13 +2,26 @@ package me.okay.minetwitch;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.philippheuer.events4j.core.EventManager;
+import com.github.philippheuer.events4j.simple.SimpleEventHandler;
+
 import me.okay.minetwitch.command.MinetwitchCommand;
 public class Minetwitch extends JavaPlugin {
     private TwitchBot twitchBot;
+    private Database database;
+
+    private EventManager twitch4jEventManager;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
+
+        // This is here to work around a strange issue that has to do with random errors with the twitch4j library and spigot.
+        twitch4jEventManager = new EventManager();
+        twitch4jEventManager.registerEventHandler(new SimpleEventHandler());
+        twitch4jEventManager.setDefaultEventHandler(SimpleEventHandler.class);
+
+        database = new Database(this);
 
         twitchBot = new TwitchBot(this);
         
@@ -19,6 +32,11 @@ public class Minetwitch extends JavaPlugin {
     @Override
     public void onDisable() {
         twitchBot.disconnect();
+        twitch4jEventManager.close();
+    }
+
+    public Database getDatabase() {
+        return database;
     }
 
     public void reloadTwitchBot() {
